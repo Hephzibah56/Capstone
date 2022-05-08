3b4b77f0cb6283ab8e18335bfd549f962e32cafc
Hephzibah Emereole <43935697+Hephzibah56@users.noreply.github.com>
2022-05-08 20:02:10 +0000

    E-learning system

---
+++ main2.py
@@ -0,0 +1,27 @@
+from fastapi import BackgroundTasks, FastAPI
+from pygtail import Pygtail
+
+import utility
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root(background_tasks: BackgroundTasks):
+    background_tasks.add_task(utility.main)
+    return "transcription service initiated"
+
+
+@app.get("/speak")
+async def speak():
+    # transcript_arr = []
+    transcript_dict = {}
+
+    for line in Pygtail("transcribed.txt"):
+        line = line.split(":")
+        # # array
+        # transcript_arr.append(line[1])
+
+        #dict
+        transcript_dict[line[0]] = line[1]
+    return transcript_dict
